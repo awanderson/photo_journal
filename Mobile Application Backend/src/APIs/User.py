bce4f22d21c9b2a2b9823eb5c1fc29bb015685fe
Alec Anderson <alecanderson12@gmail.com>
2013-07-25 18:47:32 -0500

    User stuff

--- Mobile Application Backend/src/APIs/User.py
+++ Mobile Application Backend/src/APIs/User.py
@@ -4,33 +4,25 @@ Created on Jul 10, 2013
 @author: jacobforster
 '''
 
-#TEST GITHUB COMMIT test again third test fourth test
 #development api explorer located at localhost:8080/_ah/api/explorer - uses googles own server and passes your localhost as the base so basically accessing your locahost from google's server.. nice
 from google.appengine.ext import ndb
 from protorpc import messages
 from protorpc import remote
 from google.appengine.ext import endpoints
 #from webapp2_extras.appengine.auth import models
-from webapp2_extras import security
-from webapp2_extras import sessions
-from webapp2_extras import auth 
 from Classes import user
-
-#this allows python to search classes folder
-#added line comment 20
-import os
-#sys.path.insert(0, '/_ah/api/Classes')
-
-#imports custom user class from classes folder
-#import user
-
 import time
+import logging
+from webapp2_extras import auth
+from webapp2_extras.auth import InvalidAuthIdError
+from webapp2_extras.auth import InvalidPasswordError
+
 
 #this is the actual object (message) that we will be transferring
 class UserMessage(messages.Message):
-    username = messages.StringField(1, required=True)
+    username = messages.StringField(1, required=False)
     password = messages.StringField(2, required=True)
-    email = messages.StringField(3, required=True)
+    email = messages.StringField(3, required=False)
     user_id = messages.IntegerField(4, required=False)
     auth_token = messages.StringField(5, required=False)
     error_message = messages.StringField(6, required=False)
@@ -38,23 +30,33 @@ class UserMessage(messages.Message):
     
     
     #need a name for service, version number? and human readable description
-@endpoints.api(name='userService', version='v0.1915', description='API for User methods', hostname='engaged-context-254.appspot.com')    
+@endpoints.api(name='userService', version='v0.1916', description='API for User methods', hostname='engaged-context-254.appspot.com')  
 class UserApi(remote.Service):
+    
+    
     
     
     #this method signs the user up, pretty self explanatory
     @endpoints.method(UserMessage, UserMessage, name='User.signup', path='signup', http_method='POST')
     def SignupUser(self, request):
         
+        #makes sure required fields aren't blank
+        if (request.username == "") or (request.password == "") or (request.email == ""):
+        
+            request.error_message = "Missing a required field"
+            request.error_number = 11
+            return request
+        
         #sets properties which have to be unique, otherwise won't create user
-        unique_properties = ['email_address', 'username']
-        user_name = request.username
+        unique_properties = ['email_address']
+        
+        username = request.username
         password = request.password
         email = request.email
         
-        #actually creates the user, inserts into db
-        user_data = user.User.create_user(user_name, unique_properties, email_address = email, password_raw=password)
         
+        #actually creates the user, inserts into db
+        user_data = user.User.create_user(username, unique_properties, email_address = email, password_raw=password, user_name=username)
         
         #this means email is already registered, send back error message
         if not user_data[0]:
@@ -62,22 +64,40 @@ class UserApi(remote.Service):
             request.error_number = 10
             return request
         
+        #gets user data if successful, variable can't be user, causes error
+        user_ob = user_data[1]
         
-        #gets user data
-        #user = user_data[1]
-        
-        """
         #gets user id to use in other api calls
-        request.user_id = user.get_id()
+        request.user_id = user_ob.get_id()
         
         #creates user token
         request.auth_token = user.User.create_auth_token(request.user_id)
         
-        """
         
         return request
-       
-
-
-#app = endpoints.api_server([UserApi], restricted=False)
-
+    
+    #login method using password and email/username
+    @endpoints.method(UserMessage, UserMessage, name='User.login', path='login', http_method='POST')
+    def LoginUser(self, request):
+        
+        logging.getLogger().setLevel(logging.DEBUG)
+        
+        #tries using email password combo first
+        username = request.username
+        password = request.password
+        try:
+            #auth_ob = auth.Auth(request)
+            user_ob = auth.Auth.get_user_by_password(username, password)
+            request.error_message = str(user_ob)
+        except (InvalidAuthIdError, InvalidPasswordError) as e:
+            logging.error("test")
+            request.error_message = "issue logging in user"
+        
+        """
+        try:
+            
+        except (InvalidAuthIdError, InvalidPasswordError) as e:
+            #request.error_message = e;
+            return request
+        """
+        return request
