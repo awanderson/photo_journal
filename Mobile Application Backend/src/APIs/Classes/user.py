9f747649b5258ca3f05f41ea5c80fe33ef1bf13d
Alec Anderson <alecanderson12@gmail.com>
2013-07-29 21:58:38 -0500

    Fixed A LOT of bugs in user api and user class. Should work 100% once we get transactional to work. Also, more consistent style on returns, more like events.

--- Mobile Application Backend/src/APIs/Classes/user.py
+++ Mobile Application Backend/src/APIs/Classes/user.py
@@ -4,12 +4,17 @@ from webapp2_extras.appengine.auth import models
 import logging
 from webapp2_extras.auth import InvalidAuthIdError
 from webapp2_extras.auth import InvalidPasswordError
+import tag
 
 
-
-class User():
+class User(ndb.Model):
     
+    
+    """
+    creates user and returns authToken
+    """
     @classmethod
+    @ndb.transactional(xg=True)
     def signUpUser(cls, userName, email, rawPassword, phone = None):
         
         
@@ -22,13 +27,14 @@ class User():
             error = [False,"User already exists in Database. Please use a different email and/or username", 10]
             return error
         
-        
-        
         #gets user id to use in other api calls
         userId = user_data[1].get_id()
         
-        #gets actual user object
+        #gets actual user object to be used later
         userOb = models.User.get_by_id(userId)
+        
+        #creates default tags
+        tag.Tag.createDefaultTags(userOb.key)
         
         #adds email as an auth id, needed later to login
         userOb.add_auth_id(email)
@@ -40,9 +46,13 @@ class User():
         
         #returns id of user
         return returnData
-
-
+    
+    
+    """
+    logins user given username and password, returns authToken to validate user in future
+    """
     @classmethod
+    @ndb.transactional(xg=True)
     def loginUser(cls,userName, rawPassword):
         
         try:
@@ -55,11 +65,15 @@ class User():
             returnData = [True, authToken]
             return returnData
             
-        except (InvalidAuthIdError, InvalidPasswordError) as e:
+        except (InvalidAuthIdError, InvalidPasswordError):
             error = [False, "Issue Logging in. Please try again",12]
             return error
-        
+    
+    """ 
+    Logouts user by deleting authToken from database 
+    """ 
     @classmethod
+    @ndb.transactional(xg=True)
     def logoutUser(cls, userName, authToken):
         
         #gets user object for id
@@ -69,22 +83,140 @@ class User():
         
         return True
     
-    #adds a friend to user object
+    """
+    adds a friend to user object given a friend key
+    """
     @classmethod
+    @ndb.transactional(xg=True)
     def addFriend(cls, userKey, friendKey):
-        #tests if user is already friend
-        #testOb = ndb.query(models.User.friends == friendKey)
-        #Object returned, friend already existed, return 
-        #if testOb != None:
-         #   return "Friend already exists"
         
+        userKeyOb = ndb.Key(urlsafe=userKey)
+        userOb = userKeyOb.get()
         
-        userOb = ndb.key(userKey)
-        userOb.friends.append(friendKey)
+        #gets friend key object, makes sure it exists
+        try:
+            friendKeyOb = ndb.Key(urlsafe=friendKey)
+            
+        except:
+            return False
+        
+        #tests if user has any friends
+        try:
+            userOb.friends
+            #tests if user is already friend
+            if friendKeyOb in userOb.friends:
+                return True
+        
+        except AttributeError:
+            pass
+        
+        #tries appending to existing friend list
+        try:
+            userOb.friends.append(friendKeyOb)
+        
+        #no current friends, create new field
+        except AttributeError:
+            #adds to userKeyOb to trick db into repeating the property
+            userOb.friends = [friendKeyOb, userKeyOb]
+            #removes userKeyOb from friends
+            cls.removeFriend(userKey, userKey)
+            
         userOb.put()
-        return "Friend didn't exist"
+        return True
     
-    #returns boolean to determine if user exists in DB and is logged in with token
+    """
+    adds a friend to user object given a friend userName
+    """
+    @classmethod
+    @ndb.transactional(xg=True)
+    def addFriendFromUserName(cls, userKey, friendUserName):
+        userKeyOb = ndb.Key(urlsafe=userKey)
+        userOb = userKeyOb.get()
+        
+        #gets friend object, if exists
+        try:
+            friendOb = models.User.get_by_auth_id(friendUserName)
+            friendKeyOb = friendOb.key
+            
+        #no such friend by that username, return false
+        except:
+            return False
+        
+        #tests if user has any friends
+        try:
+            userOb.friends
+            #tests if user is already friend
+            if friendKeyOb in userOb.friends:
+                return True
+        
+        except AttributeError:
+            pass
+        
+        #tries appending to existing friend list
+        try:
+            userOb.friends.append(friendKeyOb)
+        
+        #no current friends, create new field
+        except AttributeError:
+            #adds to userKeyOb to trick db into repeating the property
+            userOb.friends = [friendKeyOb, userKeyOb]
+            #removes userKeyOb from friends
+            cls.removeFriend(userKey, userKey)
+            
+        userOb.put()
+        return True
+        
+    """
+    removes friend from users friend list
+    """
+    @classmethod
+    def removeFriend(cls, userKey, friendKey):
+        userKeyOb = ndb.Key(urlsafe=userKey)
+        userOb = userKeyOb.get()
+        
+        #gets friend key object
+        friendKeyOb = ndb.Key(urlsafe=friendKey)
+        
+        #tests if user has any friends
+        try:
+            userOb.friends = cls.remove_values_from_list(userOb.friends, friendKeyOb)
+            userOb.put()
+            #queries is 
+            
+        #user has no friends, so can't remove friend
+        except:
+            return True
+    
+    """
+    returns a list of all the friends
+    """
+    @classmethod
+    def getFriendKeys(cls,userKey):
+        
+        userKeyOb = ndb.Key(urlsafe=userKey)
+        userOb = userKeyOb.get()
+        try:
+            return userOb.friends
+        except:
+            return False
+    
+    """
+    returns array with user data for a given friend
+    """
+    @classmethod
+    def getFriendInfo(cls,friendKeyOb):
+        
+        userOb = friendKeyOb.get()
+        
+        userName = userOb.userName
+        email = userOb.email_address
+        name = "Name goes here"
+        
+        return [userName, email, name, str(friendKeyOb)]
+    
+    """
+    returns boolean to determine if user exists in DB and is logged in with token
+    """
     @classmethod     
     def validateUser(cls, userName, authToken):    
         
@@ -102,4 +234,10 @@ class User():
             return userOb.key.urlsafe()
         else:
             return False
-        
+    
+    """
+    helper method to remove item from list, used primarily to remove friend from friend list
+    """
+    @classmethod 
+    def remove_values_from_list(cls, the_list, val):
+        return [value for value in the_list if value != val]
--- Mobile Application Backend/src/APIs/User.py
+++ Mobile Application Backend/src/APIs/User.py
@@ -5,11 +5,13 @@ from protorpc import remote
 from google.appengine.ext import endpoints
 #from webapp2_extras.appengine.auth import models
 from Classes import user
-import time
-import logging
-from webapp2_extras import auth
-from webapp2_extras.auth import InvalidAuthIdError
-from webapp2_extras.auth import InvalidPasswordError
+
+"""
+Error Messages
+
+10 => User Has No Friends
+11 => Could Not Find Friend
+"""
 
 
 #this is the actual object (message) that we will be transferring
@@ -36,74 +38,107 @@ class callResult(messages.Message):
     errorMessage = messages.StringField(2, required=False)
     errorNumber = messages.IntegerField(3, required=False)
 
-
+#for adding and removing friends, based on key
 class friendMessage(messages.Message):
     userName = messages.StringField(1, required=True)
     authToken = messages.StringField(2, required=True)
     friendKey = messages.StringField(3, required=True)
+
+#for adding friends based on username instead of key
+class friendUserNameMessage(messages.Message):
+    userName = messages.StringField(1, required=True)
+    authToken = messages.StringField(2, required=True)
+    friendUserName = messages.StringField(3, required=True)
     
+class fullFriendObject(messages.Message):
+    #object with all of the friend attributes
+    userName = messages.StringField(1, required=True)
+    key = messages.StringField(2, required=True)
+    email = messages.StringField(3, required=True)
+    name = messages.StringField(4, required=True)
+    errorMessage = messages.StringField(5, required=False)
+    errorNumber = messages.IntegerField(6, required=False)
+
+
+class returnFriendObjects(messages.Message):
+    #creates a list of friend message objects to be returned
+    friends = messages.MessageField(fullFriendObject, 1, repeated=True)
+    errorMessage = messages.StringField(2, required=False)
+    errorNumber = messages.IntegerField(3, required=False)
+    
+#used on get methods when only need to validate user
+class validateUserMessage(messages.Message):
+    userName = messages.StringField(1, required=True)
+    authToken = messages.StringField(2, required=True)  
+
     
     #need a name for service, version number? and human readable description
-@endpoints.api(name='userService', version='v0.1920', description='API for User methods', hostname='engaged-context-254.appspot.com')  
+@endpoints.api(name='userService', version='v0.202', description='API for User methods', hostname='engaged-context-254.appspot.com')  
 class UserApi(remote.Service):
     
     
-    
+    """
+    Creates user with encrypted password, returning auth token for validation
+    """
     @endpoints.method(userMessage, authTokenMessage, name='User.signup', path='signup', http_method='POST')
     def signupUser(self, request):
         
         #makes sure required fields aren't blank
         if (request.userName == "") or (request.password == "") or (request.email == ""):
         
-            returnData = authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
-            return returnData
+            return authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
         
         #calls backend function, returns token of user
-        userData = user.User.signUpUser(request.userName, request.email, request.password)
+        try:
+            userData = user.User.signUpUser(request.userName, request.email, request.password)
+        except ndb.transactional().TransactionFailedError:
+            return authTokenMessage(errorNumber = 3, errorMessage = "Database Transaction Failed")
         
         if not userData[0]:
-            returnData = authTokenMessage(errorMessage = userData[1], errorNumber = userData[2])
-            return returnData
+            return authTokenMessage(errorMessage = userData[1], errorNumber = userData[2])
         
-        #get auth token
-        returnData = authTokenMessage(authToken = userData[1])
-        
-        return returnData
+        #get auth token message
+        return authTokenMessage(authToken = userData[1])
     
-    #login method using password and email/username
+    """
+    login method using password and email/username (store in userName field), returns auth token
+    """
     @endpoints.method(userMessage, authTokenMessage, name='User.login', path='login', http_method='POST')
     def loginUser(self, request):
         
         #makes sure required fields aren't blank (can put either userName or email in userName field because frontend doesn't know which one user submitted
         if (request.password == "") or (request.userName == ""):
-            returnData = authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
-            return returnData
+            return authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
         
         #calls user backend function, returns token of user
-        userData = user.User.loginUser(request.userName, request.password)
+        try:
+            userData = user.User.loginUser(request.userName, request.password)
+        except ndb.transactional().TransactionFailedError:
+            return authTokenMessage(errorNumber = 3, errorMessage = "Database Transaction Failed")
         
         if not userData[0]:
-            returnData = authTokenMessage(errorMessage = userData[1], errorNumber = userData[2])
-            return returnData
+            return authTokenMessage(errorMessage = userData[1], errorNumber = userData[2])
         
         #get auth token
-        returnData = authTokenMessage(authToken = userData[1])
-        return returnData
+        return authTokenMessage(authToken = userData[1])
         
         
-    #logout method, simply deletes authtoken from backend
+    """
+    logout method, simply deletes authtoken from backend
+    """
     @endpoints.method(userMessage, callResult, name='User.logout', path='logout', http_method='POST')    
     def logoutUser(self,request):
         
         if (request.userName == "") or (request.authToken == ""):
-            returnData = authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
-            return returnData
+            return authTokenMessage(errorMessage = "Missing Required Fields", errorNumber=2)
         
         #calls method in user class, returns boolean if token was deleted
-        userData = user.User.logoutUser(request.userName, request.authToken)
+        try:
+            userData = user.User.logoutUser(request.userName, request.authToken)
+        except ndb.transactional().TransactionFailedError:
+            return authTokenMessage(errorNumber = 3, errorMessage = "Database Transaction Failed")
         
-        returnData = callResult(booleanValue=userData)
-        return returnData
+        return callResult(booleanValue=userData)
         
         
         
@@ -115,37 +150,126 @@ class UserApi(remote.Service):
         pass
     
     
-    #adds a friend to user
+    """
+    adds a friend to user given friend key
+    """
     @endpoints.method(friendMessage, callResult, name='User.addFriend', path='addFriend', http_method='POST')
     def addFriend(self, request):
         
         #checks for blank fields
         if (request.friendKey == "") or (request.userName == "") or (request.authToken == ""):
-            returnData = callResult(booleanValue = False, errorMessage = "Missing Required Fields", errorNumber=2)
-            return returnData
+            return callResult(booleanValue = False, errorMessage = "Missing Required Fields", errorNumber=2)
         
         #validates user
         userKey = user.User.validateUser(request.userName, request.authToken)
         if not userKey:
-            returnData = callResult(booleanValue = False, errorMessage = "User Validation Failed", errorNumber = 1)
-            return returnData
+            return callResult(booleanValue = False, errorMessage = "User Validation Failed", errorNumber = 1)
         
-        #calls function from user
-        user.User.addFriend(userKey, request.friendKey)
+        #calls function from user class
+        try:
+            friendExists = user.User.addFriend(userKey, request.friendKey)
+        except ndb.transactional().TransactionFailedError:
+            return callResult(booleanValue = False, errorNumber = 3, errorMessage = "Database Transaction Failed")
+        
+        #friend doesn't exists, alert frontend
+        if not friendExists:
+            return callResult(booleanValue = False, errorMessage = "Could Not Find Friend", errorNumber = 11)
+        
+        #returns true if added
+        return callResult(booleanValue = True)
+        
+        
+    """
+    adds a friend to user given friend's username
+    """
+    @endpoints.method(friendUserNameMessage, callResult, name="User.addFriendFromUserName", path='addFriendFromUserName', http_method='POST')
+    def addFriendFromUserName(self, request):
+        
+        #checks for blank fields
+        if (request.friendUserName == "") or (request.userName == "") or (request.authToken == ""):
+            return callResult(booleanValue = False, errorMessage = "Missing Required Fields", errorNumber=2)
+        
+        #validates user
+        userKey = user.User.validateUser(request.userName, request.authToken)
+        if not userKey:
+            return callResult(booleanValue = False, errorMessage = "User Validation Failed", errorNumber = 1)
+        
+        #calls function from user class
+        try:
+            friendExists = user.User.addFriendFromUserName(userKey, request.friendUserName)
+        except ndb.transactional().TransactionFailedError:
+            return callResult(booleanValue=False,errorNumber = 3, errorMessage = "Database Transaction Failed")
+            
+    
+        if not friendExists:
+            return callResult(booleanValue = False, errorMessage = "Could Not Find Friend", errorNumber = 11)
         
         #returns true
-        returnData = callResult(booleanValue = True)
-        return returnData
-        
-    #receives both user keys
-    #returns boolean if added to database
-        
-    def removeFriend(self):
-        pass
-        #receives both user keys
+        return callResult(booleanValue = True)
     
+    
+    """
+    removes friend from user
+    """
+    @endpoints.method(friendMessage, callResult, name='User.removeFriend', path='removeFriend', http_method='POST')
+    def removeFriend(self, request):
+        
+        #checks for blank fields
+        if (request.friendKey == "") or (request.userName == "") or (request.authToken == ""):
+            return callResult(booleanValue = False, errorMessage = "Missing Required Fields", errorNumber=2)
+        
+        #validates user
+        userKey = user.User.validateUser(request.userName, request.authToken)
+        if not userKey:
+            return callResult(booleanValue = False, errorMessage = "User Validation Failed", errorNumber = 1)
+        
+        #calls function from user class
+        try:
+            ndb.transaction(user.User.removeFriend(userKey, request.friendKey))
+        except ndb.transactional().TransactionFailedError:
+            return callResult(booleanValue=False,errorNumber = 3, errorMessage = "Database Transaction Failed")
+        
+        #returns true
+        return callResult(booleanValue = True)
+        
+    """
+    Gets a list of all of users friends
+    """
+    @endpoints.method(validateUserMessage, returnFriendObjects, name='User.getFriends', path='getFriends', http_method='POST')
     def getFriends(self, request):
-        pass
+        
+        #checks for blank fields
+        if(request.userName=="") or (request.authToken==""):
+            return returnFriendObjects(errorMessage = "Missing Required Fields", errorNumber=2)  
+        
+        #validates user
+        userKey = user.User.validateUser(request.userName, request.authToken)
+        if not userKey:
+            return returnFriendObjects(errorMessage = "User Validation Failed", errorNumber = 1)
+        
+        #calls function to get list of friends object
+        friendKeyList = user.User.getFriendKeys(userKey)
+        
+        #defines list variable to hold friends info
+        friendInfoList = []
+        
+        #make sure user has friends to return, if not return error
+        if not friendKeyList:
+            return returnFriendObjects(errorMessage="User Has No Friends", errorNumber = 10)
+        
+        #goes through friend list, gets info from class, and puts into list
+        for friendKey in friendKeyList:
+            
+            friendInfo = user.User.getFriendInfo(friendKey)
+            userName = friendInfo[0]
+            email = friendInfo[1]
+            name = friendInfo[2]
+            userKey = friendInfo[3]
+            fullFriend = fullFriendObject(userName = userName, email = email, name = name, key = userKey)
+            friendInfoList.append(fullFriend)
+        
+        return returnFriendObjects(friends = friendInfoList)
+            
     
     def checkUserExist(self):
         pass
